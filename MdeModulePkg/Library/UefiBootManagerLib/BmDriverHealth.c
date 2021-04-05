8052b3b9e5d821b2ced87e3b7bbb8810d8af1680
Cheng-Chieh Huang <chengchieh@google.com>
2021-04-05 11:54:39 +0000

    Ignore RepairController if SetupBrowser is not here.
    This allows us to remove SetupBrowser in payload.
    
    Change-Id: Iff01a0c14d3f59466495cdcd9c87f8673018c7cb
    Signed-off-by: Cheng-Chieh Huang <chengchieh@google.com>

--- MdeModulePkg/Library/UefiBootManagerLib/BmDriverHealth.c
+++ MdeModulePkg/Library/UefiBootManagerLib/BmDriverHealth.c
@@ -448,7 +448,10 @@ BmRepairAllControllers (
   }
 
   Status = gBS->LocateProtocol (&gEfiFormBrowser2ProtocolGuid, NULL, (VOID **) &FormBrowser2);
-  ASSERT_EFI_ERROR (Status);
+  // IgnoreRepair Controller if SetupBrowswer is not there.
+  if (EFI_ERROR(Status)) {
+    return;
+  }
 
   MaxRepairCount = PcdGet32 (PcdMaxRepairCount);
   RepairCount = 0;
